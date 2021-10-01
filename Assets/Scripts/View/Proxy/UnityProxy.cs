7992e5dd26ec325c77c469bc4818a16a0033f03a
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-10-01 13:41:39 +0300

    Update UnityProxy.cs

--- Assets/Scripts/View/Proxy/UnityProxy.cs
+++ Assets/Scripts/View/Proxy/UnityProxy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace QuoridorDelta.View.Proxy
@@ -7,6 +8,10 @@ namespace QuoridorDelta.View.Proxy
     {
         // todo
         //[SerializeField] private FakeView _fakeView;
+
+        private bool _startedMoveTypeInitialization = false;
+        private bool _startedMovePawnInitialization = false;
+        private bool _startedPlaceWallInitialization = false;
 
         private bool _isAlive = true;
         private QuoridorProxy _proxy;
@@ -37,19 +42,22 @@ namespace QuoridorDelta.View.Proxy
             while (_isAlive)
             {
                 // todo
-                if (_proxy.MoveTypeRequest != null && !_proxy.MoveTypeRequest.Initialized)
+                if (_proxy.MoveTypeRequest != null && !_proxy.MoveTypeRequest.Initialized && !_startedMoveTypeInitialization)
                 {
-                    _view.GetMoveType(_proxy.MoveTypeRequest.Input, _proxy.MoveTypeRequest.Initialize);
+                    _startedMoveTypeInitialization = true;
+                    _view.GetMoveType(_proxy.MoveTypeRequest.Input, handle => { _startedMoveTypeInitialization = false; _proxy.MoveTypeRequest.Initialize(handle); });
                     Debug.Log($"Initialized MoveTypeRequest");
                 }
-                else if (_proxy.MovePawnRequest != null && !_proxy.MovePawnRequest.Initialized)
+                else if (_proxy.MovePawnRequest != null && !_proxy.MovePawnRequest.Initialized && !_startedMovePawnInitialization)
                 {
-                    _view.GetMovePawnCoords(_proxy.MovePawnRequest.Input, _proxy.MovePawnRequest.Initialize);
+                    _startedMovePawnInitialization = true;
+                    _view.GetMovePawnCoords(_proxy.MovePawnRequest.Input, handle => { _startedMovePawnInitialization = false; _proxy.MovePawnRequest.Initialize(handle); });
                     Debug.Log($"Initialized MovePawnRequest");
                 }
-                else if (_proxy.PlaceWallRequest != null && !_proxy.PlaceWallRequest.Initialized)
+                else if (_proxy.PlaceWallRequest != null && !_proxy.PlaceWallRequest.Initialized && !_startedPlaceWallInitialization)
                 {
-                    _view.GetPlaceWallCoords(_proxy.PlaceWallRequest.Input, _proxy.PlaceWallRequest.Initialize);
+                    _startedMovePawnInitialization = true;
+                    _view.GetPlaceWallCoords(_proxy.PlaceWallRequest.Input, handle => { _startedMovePawnInitialization = false; _proxy.PlaceWallRequest.Initialize(handle); });
                     Debug.Log($"Initialized PlaceWallRequest");
                 }
 
