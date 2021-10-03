ec6669b8240163c662d76c07744f0bd68a09462e
nekt2111 <54265542+nekt2111@users.noreply.github.com>
2021-10-03 14:31:35 +0300

    basic logic

--- Assets/Scripts/Controller/Controller.cs
+++ Assets/Scripts/Controller/Controller.cs
@@ -4,6 +4,23 @@ namespace QuoridorDelta.Controller
 {
     class Controller
     {
+        public void Run()
+        {
+            
+            // Start Loop1
+            // Play duo or with bot
+
+            // start Loop2 -> what is a moveType, what coords, tryToMove -> (try again,move), moved
+            // Next player -> what is a moveType, what coords, tryToMove -> (try again,move), moved
+            // While flag won == true
+            // Winner is -> end Loop2
+
+            // Do you want to play again 
+            // If yes Loop1 doesn`t end, If no Loop1 ends
+
+
+
+        }
       
     }
 }
