89ed5d48f4ca68d34d1cb9381c7c0a4fc5eee37e
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-10-04 15:29:28 +0300

    Bot fin

--- Assets/Scripts/AI/RandomBot.cs
+++ Assets/Scripts/AI/RandomBot.cs
@@ -5,30 +5,19 @@ using System.Linq;
 
 namespace QuoridorDelta.View
 {
-    public sealed class RandomBot : IBot
+    public sealed class RandomBot : IInput
     {
         private readonly Random _random = new Random();
 
         public Coords GetMovePawnCoords(PlayerType playerType, IEnumerable<Coords> possibleMoves)
-            => GetRandomPawnCoords(possibleMoves.ToArray());
+            => GetRandom(possibleMoves.ToArray());
 
         public MoveType GetMoveType(PlayerType playerType) => (MoveType) _random.Next(0, 2);
 
         public WallCoords GetPlaceWallCoords(PlayerType playerType, IEnumerable<WallCoords> possibleWallPlacements)
-            => GetRandomWallCoords(possibleWallPlacements.ToArray());
+            => GetRandom(possibleWallPlacements.ToArray());
 
-        public Coords GetRandomPawnCoords(Coords[] possibleCoords)
-        {
-            int rInt = _random.Next(0, possibleCoords.Length);
-
-            return possibleCoords[rInt];
-        }
-
-        public WallCoords GetRandomWallCoords(WallCoords[] possibleWallPlacementCoords)
-        {
-            int rInt = _random.Next(0, possibleWallPlacementCoords.Length);
-
-            return possibleWallPlacementCoords[rInt];
-        }
+        private T GetRandom<T>(IReadOnlyList<T> possibleCoords)
+            => possibleCoords[_random.Next(0, possibleCoords.Count)];
     }
 }
