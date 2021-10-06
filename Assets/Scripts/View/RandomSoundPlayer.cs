b6aa7b685e9b0073c1332c18a48a9a154ebe3aa8
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-10-06 15:41:58 +0300

    added sounds

---
+++ Assets/Scripts/View/RandomSoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = System.Random;
+
+namespace QuoridorDelta.View
+{
+    [RequireComponent(typeof(AudioSource))]
+    public sealed class RandomSoundPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioClip[] _clips;
+
+        private readonly Random _random = new Random();
+        private AudioSource _source;
+
+        private void Awake() => _source = GetComponent<AudioSource>();
+
+        public void PlayNext()
+        {
+            _source.clip = _clips[_random.Next(_clips.Length)];
+            _source.Play();
+        }
+    }
+}
