22ac19836a6509b4af4fe4e6d15bb23e86a78442
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-10-06 12:54:21 +0300

    Added Highlitable support

---
+++ Assets/Scripts/View/Highlitable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace QuoridorDelta.View
+{
+    public sealed class Highlitable : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer _renderer;
+        [FormerlySerializedAs("_chosenMaterial")] [SerializeField] private Material _changedMaterial;
+
+        private Material[] _defaultMaterials;
+        private Material[] _changedMaterials;
+
+        private void Awake()
+        {
+            _defaultMaterials = _renderer.materials;
+            _changedMaterials = new List<Material>(_renderer.materials) { _changedMaterial }.ToArray();
+        }
+
+        public void Change(bool highlited) => _renderer.materials = highlited ? _changedMaterials : _defaultMaterials;
+    }
+}
