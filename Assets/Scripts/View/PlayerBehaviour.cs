2e471582ff1c2036430e5144a6c71a97352584a5
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-10-06 14:11:20 +0300

    merge fix

--- Assets/Scripts/View/PlayerBehaviour.cs
+++ Assets/Scripts/View/PlayerBehaviour.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-<<<<<<< HEAD
 using System.Collections.Generic;
-=======
 using JetBrains.Annotations;
->>>>>>> 43b2059e33688bb65dfb4b6dfc51ff028b677ed0
 using QuoridorDelta.Model;
 using UnityEngine;
 
@@ -20,6 +17,7 @@ namespace QuoridorDelta.View
 
 
         private CoordsConverter _coordsConverter;
+
         private Dictionary<PlayerInfo, List<WallGameObject>> _playerWallsList;
         //private readonly Vector3 _firstWall = new Vector3(-4.5f, 0.915f, -5.5f);
 
@@ -31,7 +29,6 @@ namespace QuoridorDelta.View
         public void Start()
         {
             _coordsConverter = _view.CoordsConverter;
-            
         }
 
 
@@ -40,22 +37,36 @@ namespace QuoridorDelta.View
             _playerWallsList = new Dictionary<PlayerInfo, List<WallGameObject>>();
             _playerWallsList[playerInfos.First] = new List<WallGameObject>();
             _playerWallsList[playerInfos.Second] = new List<WallGameObject>();
+
             for (int i = 0; i < playerInfos.First.WallCount; i++)
             {
-                Vector3 coords = new Vector3(_coordsConverter.CenterPoint.x - 4.5f + i, 0.915f, _coordsConverter.CenterPoint.z - 5.5f);
-                WallGameObject wall = new WallGameObject(Instantiate(_wallPrefab, coords, Quaternion.identity, _wallsParent.transform), coords);
+                Vector3 coords = new Vector3(_coordsConverter.CenterPoint.x - 4.5f + i,
+                                             0.915f,
+                                             _coordsConverter.CenterPoint.z - 5.5f);
+
+                WallGameObject wall =
+                    new WallGameObject(Instantiate(_wallPrefab, coords, Quaternion.identity, _wallsParent.transform),
+                                       coords);
+
                 _playerWallsList[playerInfos.First].Add(wall);
             }
 
             for (int i = 0; i < playerInfos.Second.WallCount; i++)
             {
-                Vector3 coords = new Vector3(_coordsConverter.CenterPoint.x - 4.5f + i, 0.915f, _coordsConverter.CenterPoint.z + 5.5f);
-                WallGameObject wall = new WallGameObject(Instantiate(_wallPrefab, coords, Quaternion.identity, _wallsParent.transform), coords);
+                Vector3 coords = new Vector3(_coordsConverter.CenterPoint.x - 4.5f + i,
+                                             0.915f,
+                                             _coordsConverter.CenterPoint.z + 5.5f);
+
+                WallGameObject wall =
+                    new WallGameObject(Instantiate(_wallPrefab, coords, Quaternion.identity, _wallsParent.transform),
+                                       coords);
+
                 _playerWallsList[playerInfos.Second].Add(wall);
             }
 
             IsInitialized = true;
         }
+
         private GameObject GetPawn(PlayerNumber playerNumber) => playerNumber switch
         {
             PlayerNumber.First => _pawn1,
@@ -67,12 +78,10 @@ namespace QuoridorDelta.View
         {
             // todo: add animations
             Transform pawnTransform = GetPawn(playerNumber).transform;
-            
-            _animations.Move(pawnTransform, 
-                             pawnTransform.position,
-                             _coordsConverter.ToVector3(newCoords),
-                             finHandler);
+
+            _animations.Move(pawnTransform, pawnTransform.position, _coordsConverter.ToVector3(newCoords), finHandler);
         }
+
         public void PlaceWall(PlayerInfos playerInfos, PlayerNumber playerNumber, WallCoords newCoords)
         {
             Quaternion quaternion = newCoords.Rotation switch
@@ -84,32 +93,38 @@ namespace QuoridorDelta.View
 
             PlayerInfo playerInfo;
             int lastFreeWallIndex;
+
             switch (playerNumber)
             {
                 case PlayerNumber.First:
                     playerInfo = playerInfos.First;
                     lastFreeWallIndex = _lastFreeWallIndexInFirst++;
+
                     break;
                 case PlayerNumber.Second:
                     playerInfo = playerInfos.Second;
                     lastFreeWallIndex = _lastFreeWallIndexInSecond++;
+
                     break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                default: throw new ArgumentOutOfRangeException();
             }
 
-            _playerWallsList[playerInfo][lastFreeWallIndex].PlaceWallGameObject(_coordsConverter.ToVector3(newCoords), quaternion);
+            _playerWallsList[playerInfo][lastFreeWallIndex]
+                .PlaceWallGameObject(_coordsConverter.ToVector3(newCoords), quaternion);
         }
+
         public void ResetWallsPosition(PlayerInfos playerInfos)
         {
             if (IsInitialized == false)
             {
                 InitializePlayerWalls(playerInfos);
             }
+
             foreach (WallGameObject wall in _playerWallsList[playerInfos.First])
             {
                 wall.ResetToStartPosition();
             }
+
             foreach (WallGameObject wall in _playerWallsList[playerInfos.Second])
             {
                 wall.ResetToStartPosition();
--- Assets/Scripts/View/View.cs
+++ Assets/Scripts/View/View.cs
@@ -139,11 +139,7 @@ namespace QuoridorDelta.View
             PlayerNumber playerNumber,
             Coords newCoords
         )
-<<<<<<< HEAD
-            => _playerBehaviour.MovePawn(playerNumber, newCoords);
-=======
-            => _pawnBehaviour.MovePawn(playerNumber, newCoords, _proxy.HandleAnimationEnded);
->>>>>>> 43b2059e33688bb65dfb4b6dfc51ff028b677ed0
+            => _playerBehaviour.MovePawn(playerNumber, newCoords, _proxy.HandleAnimationEnded);
 
         public void PlaceWall(
             PlayerInfos playerInfos,
