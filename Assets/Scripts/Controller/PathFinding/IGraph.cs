c82672d9bef8fdd85f558bec8ad485dc1f33001a
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-11-03 20:35:31 +0200

    Path Finder Test

--- Assets/Scripts/Controller/PathFinding/IGraph.cs
+++ Assets/Scripts/Controller/PathFinding/IGraph.cs
@@ -1,4 +1,7 @@
-﻿#nullable enable
+﻿using System;
+using System.Collections.Generic;
+
+#nullable enable
 
 namespace QuoridorDelta.Controller.PathFinding
 {
@@ -8,19 +11,49 @@ namespace QuoridorDelta.Controller.PathFinding
         INode FirstNode { get; }
     }
 
-    public sealed class Test<TPathFinder> where TPathFinder : IPathFinder, new()
+    public sealed class Tester<TPathFinder> where TPathFinder : IPathFinder, new()
     {
+        public class Node : INode
+        {
+            public IEnumerable<INode> Neighbors { get; set; }
+            public (int x, int y) Position { get; set; }
+        }
+        
         public class Graph : IGraph
         {
+            public INode[] Nodes { get; }
+            public INode FirstNode { get; }
             
+            public Graph(INode[] nodes, INode firstNode)
+            {
+                Nodes = nodes;
+                FirstNode = firstNode;
+            }
         }
         
         public void Test()
         {
             IPathFinder finder = new TPathFinder();
+
+            Node node1 = new Node();
+            Node node2 = new Node();
+            Node node3 = new Node();
+            Node node4 = new Node();
+
+            node1.Neighbors = new[] { node2, node4 };
+            node1.Position = (5, 7);
+            node2.Neighbors = new[] { node1, node3 };
+            node2.Position = (6, 7);
+            node3.Neighbors = new[] { node2, node4 };
+            node3.Position = (7, 7);
+            node4.Neighbors = new[] { node3 };
+            node4.Position = (7, 8);
+
+            IGraph graph = new Graph(new INode[] { node1, node2, node3, node4 }, node1);
+
+            int result = finder.GetShortestPathLength(graph);
             
-            
-            finder.GetShortestPathLength()
+            Console.WriteLine(result);
         }
     }
 }
